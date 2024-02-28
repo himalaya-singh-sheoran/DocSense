60500ed716439382e26b6101460481379d84b108
Himalaya Sheoran <sheoran26800@gmail.com>
2024-02-28 12:33:30 +0530

    Update adhoc.py

--- adhoc.py
+++ adhoc.py
@@ -1,18 +1,21 @@
 import cv2
+import numpy as np
 
 def enhance_image(image):
-    # Denoise the image
-    denoised_image = cv2.fastNlMeansDenoisingColored(image, None, 10, 10, 7, 21)
-    
-    # Enhance contrast
-    lab = cv2.cvtColor(denoised_image, cv2.COLOR_BGR2LAB)
-    l, a, b = cv2.split(lab)
-    clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8))
-    l_enhanced = clahe.apply(l)
-    lab_enhanced = cv2.merge((l_enhanced, a, b))
-    contrast_enhanced_image = cv2.cvtColor(lab_enhanced, cv2.COLOR_LAB2BGR)
-    
-    return contrast_enhanced_image
+    # Apply gamma correction
+    gamma = 1.5
+    corrected_image = np.uint8(cv2.pow(image / 255.0, gamma) * 255)
+
+    # Apply histogram equalization
+    gray_image = cv2.cvtColor(corrected_image, cv2.COLOR_BGR2GRAY)
+    equalized_image = cv2.equalizeHist(gray_image)
+    equalized_image = cv2.cvtColor(equalized_image, cv2.COLOR_GRAY2BGR)
+
+    # Apply sharpening
+    kernel_sharpening = np.array([[-1,-1,-1], [-1, 9,-1], [-1,-1,-1]])
+    sharpened_image = cv2.filter2D(equalized_image, -1, kernel_sharpening)
+
+    return sharpened_image
 
 # Load the image
 image = cv2.imread('input_image.jpg')
